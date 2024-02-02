548b9a4f69b0d21cb1f113027098b4148c376cef
undscr <jgttpmail@gmail.com>
2024-02-01 22:57:21 -0600

    sdffdd

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import sharp from "sharp";
 import fs from "fs/promises";
 import readline from "readline/promises";
+import createLogger from "progress-estimator"
+
+// Constants
+const CHANNELS = 3;
+const logger = createLogger();
 
 // Create interface for user input
 const rl = readline.createInterface({
@@ -9,32 +14,19 @@ const rl = readline.createInterface({
 });
 
 async function createImage(buffer: Buffer, outputPath: string) {
-    //const length = Math.floor(buffer.length/2);
+    const size = Math.floor(Math.sqrt(Math.floor(buffer.byteLength / CHANNELS)));
+    const maxBufferLength = size * size * CHANNELS;
+    buffer = Buffer.from([...buffer].slice(0, maxBufferLength));
 
-    const bufferTest = Buffer.from([
-        255, 0, 0,  // Red pixel
-        0, 255, 0,  // Green pixel
-        0, 0, 255,  // Blue pixel
-        255, 255, 0,  // Yellow pixel
-        0, 255, 255,  // Cyan pixel
-        255, 0, 255,  // Magenta pixel
-        255, 255, 255,  // White pixel
-        0, 0, 0,  // Black pixel
-        127, 127, 127,  // Gray pixel
-    ]);
-
-    const size = Math.floor(Math.sqrt(buffer.length / 3));
-    console.log(buffer.length, size);
-
-    const imageBuffer = await sharp(buffer, { raw: { width: size, height: size, channels: 3 } })
-        .grayscale()
-        .toBuffer();
+    const imageBuffer = await logger(sharp(buffer, { raw: { width: size, height: size, channels: CHANNELS } }).toFormat("jpeg").toBuffer(), "Creating image");
     
-    return fs.writeFile(outputPath + ".jpg", imageBuffer);
+    return fs.writeFile(outputPath + ".jpeg", imageBuffer);
 }
 
 // Main program
 async function main() {
+    let canGenerateImage = true;
+
     // Ask user for image path and output path
     const imagePath = await rl.question("Enter the path to the image: ");
     
@@ -45,25 +37,32 @@ async function main() {
         return await main();
     }
 
+
     const outputPath = await rl.question("Enter the path to the output: ");
     const outputExists = await fs.exists(outputPath)
     if (outputPath === imagePath) {
         console.log("The output path cannot be the same as the input path.");
-        return await main();
+        canGenerateImage = false;
     } else if (outputPath === "") {
         console.log("The output path cannot be empty.");
-        return await main();
+        canGenerateImage = false;
     } else if (outputExists) {
-        console.log("The output path already exists.");
-        return await main();
+        console.log("The output path already exists as a file.");
+        canGenerateImage = false;
     }
-    const getBuffer = await fs.readFile(imagePath);
 
-    createImage(getBuffer, outputPath).then(() => {
-        console.log(`Image has been created at ${outputPath}.`);
-    }).catch((err) => {
-        console.log("An error occurred: ", err);
-    });
+    if (canGenerateImage) {
+        const getBuffer = await logger(fs.readFile(imagePath), "Reading image");
+
+        try {
+            await logger(createImage(getBuffer, outputPath), "Writing to path");
+            console.log("Image created successfully.");
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return await main();
 }
 
 main();
