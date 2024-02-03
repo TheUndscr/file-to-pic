04642023287941447984633f0f3efd56ef9a8de4
undscr <jgttpmail@gmail.com>
2024-02-03 17:03:21 -0600

    changed some stuff

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import sharp from "sharp";
-import fs from "fs/promises";
+import fs from "fs";
 import readline from "readline/promises";
-import createLogger from "progress-estimator"
+import createLogger from "progress-estimator";
 
 // Constants
 const CHANNELS = 3;
@@ -14,17 +14,15 @@ const rl = readline.createInterface({
 });
 
 // Creates an image from a buffer and outputs it to outputPath
-async function createImage(buffer: Buffer, outputPath: string) {
+function createImage(buffer: Buffer, outputPath: string) {
     const size = Math.floor(Math.sqrt(Math.floor(buffer.byteLength / CHANNELS)));
     const maxBufferLength = size * size * CHANNELS;
-    buffer = Buffer.from([...buffer].slice(0, maxBufferLength));
+    const bufferData = Buffer.from([...buffer].slice(0, maxBufferLength));
 
-    const imageBuffer = await logger(
-        sharp(buffer, { raw: { width: size, height: size, channels: CHANNELS } })
-            .toFormat("png")
-            .toBuffer(), "Creating image");
-    
-    return fs.writeFile(outputPath + ".png", imageBuffer);
+    const file = fs.createWriteStream(outputPath + ".png");
+
+    const promise = sharp(bufferData, {raw : {width: size, height: size, channels: CHANNELS}}).toFormat("png").toFile(outputPath + ".png");
+    return promise;
 }
 
 // Main program
@@ -35,15 +33,14 @@ async function main() {
     const imagePath = await rl.question("Enter the path to the image: ");
     
     // Check if the image exists
-    const imageExists = await fs.exists(imagePath);
+    const imageExists = await fs.promises.exists(imagePath);
     if (!imageExists) {
         console.log("The image does not exist.");
-        return await main();
+        return;
     }
 
-
     const outputPath = await rl.question("Enter the path to the output: ");
-    const outputExists = await fs.exists(outputPath)
+    const outputExists = await fs.promises.exists(outputPath)
     if (outputPath === imagePath) {
         console.log("The output path cannot be the same as the input path.");
         canGenerateImage = false;
@@ -57,17 +54,20 @@ async function main() {
 
     if (canGenerateImage) {
         // Get buffer from image
-        const getBuffer = await logger(fs.readFile(imagePath), "Reading image");
+        const getBuffer = await logger(fs.promises.readFile(imagePath), "Reading image");
 
         try {
-            await logger(createImage(getBuffer, outputPath), "Writing to path");
-            console.log("Image created successfully.");
+            await logger(createImage(getBuffer, outputPath), "Creating image");
         } catch (err) {
             console.error(err);
+        } finally {
+            console.log("Done.\n");
         }
     }
-
-    return await main();
 }
 
-main();
+function looper() {
+    main().then(looper);
+}
+
+looper();
